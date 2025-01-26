5befb4048be925d0c4f90aad44c5c97e2d2e121f
Nerim <nerim5037@gmail.com>
2025-01-26 16:39:22 +0900

    feat: Suspense가 작동하지 않아 제거 후 페이지 이동의 로딩 커스팀 훅을 사용하여 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,20 @@
 import "@/styles/app/reset.css";
 import type { AppProps } from "next/app";
-import { Suspense } from "react";
 import Loading from "@/components/app/Loading";
 import ScrollToTop from "@/components/app/ScrollToTop";
 import Head from "next/head";
+import { useLoading } from "@/hook/useLoading";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { loading } = useLoading();
+
   return (
     <>
       <Head>
         <link rel="icon" href="/assets/images/meta/favicon.ico" />
       </Head>
       <ScrollToTop />
-      <Suspense fallback={<Loading />}>
-        <Component {...pageProps} />
-      </Suspense>
+        {loading ? <Loading /> : <Component {...pageProps} />}
     </>
   );
 }
