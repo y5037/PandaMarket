9fdae3201afb39fa65be58835f7c1b098163e9d9
Nerim <nerim5037@gmail.com>
2025-01-26 16:41:38 +0900

    chore: useQueryBoard 커스텀 훅 이름 변경

--- hooks/useQueryBoard.ts
+++ hooks/useQueryBoard.ts
@@ -11,7 +11,7 @@ type useQueryResult<T> = {
   loading: boolean;
 };
 
-export const useQueryArticles = <T>({
+export const useQueryBoard = <T>({
   queryUrl,
   disabled = false,
 }: UseQueryProps): useQueryResult<T> => {
--- pages/boards/index.tsx
+++ pages/boards/index.tsx
@@ -4,19 +4,19 @@ import ItemListNav from "@/components/app/ItemListNav";
 import styles from "../../styles/boards/postList.module.css";
 import BestPostsList from "@/components/boards/BestPostsList";
 import AllPostsList from "@/components/boards/AllPostsList";
-import { useQueryArticles } from "@/hooks/useQueryBoard";
+import { useQueryBoard } from "@/hooks/useQueryBoard";
 import { Item } from "@/types/boards/types";
 
 function PostListPage() {
   const [order, setOrder] = useState("recent");
   const [keyword, setKeyword] = useState("");
-  const { data: likePost, loading: likeLoading } = useQueryArticles<{
+  const { data: likePost, loading: likeLoading } = useQueryBoard<{
     list: Item[];
     totalCount: number;
   }>({
     queryUrl: "articles?orderBy=like&pageSize=3",
   });
-  const { data: recentPost, loading: recentLoading } = useQueryArticles<{
+  const { data: recentPost, loading: recentLoading } = useQueryBoard<{
     list: Item[];
     totalCount: number;
   }>({
