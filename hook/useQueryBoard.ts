9b2c76556ebe6b7f6e81073edebf1feb326b38c8
Nerim <nerim5037@gmail.com>
2025-01-26 15:36:10 +0900

    chore: 데이터 유무에 따른 UI 출력의 && 연산자를 제거하니 스켈레톤 출력 이슈가 해결되어 setTimeout 제거

--- hook/useQueryBoard.ts
+++ hook/useQueryBoard.ts
@@ -26,12 +26,7 @@ export const useQueryArticles = <T>({
       } catch (err) {
         console.error(err);
       } finally {
-        // setTimeout을 적용하지 않으니 원인 불명으로 Loading 기능이 정상 작동하지 않아 api의 이미지가 불러와지는 동안 레이아웃이 하얗게 보이는 현상이 일어난다. setTimeout을 넣어주니 Loading이 정상 작동 하지만 네트워크 속도가 빠를 때에도 스켈레톤이 무조건 보이게 되어 좋은 방향은 아닌 것 같다..
-        const timer = setTimeout(() => {
-          setLoading(false);
-        }, 500);
-
-        () => clearTimeout(timer);
+        setLoading(false);
       }
     };
 
