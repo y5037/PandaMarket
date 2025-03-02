da20b6533e26a0167b7e94a66ca334dfd65481ca
Nerim <nerim5037@gmail.com>
2025-03-02 18:47:17 +0900

    chore: 기존 API 작업들 axios로 통일

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,16 +1,12 @@
+import axiosInstance from "./axios";
 import { CommentDataProps, ProductDataProps } from "./types";
-const BASE_URL = "https://panda-market-api.vercel.app";
 
 // 베스트/전체 상품 리스트
 export async function getProductData(params = {}) {
   const query = new URLSearchParams(params).toString();
-  const response = await fetch(`${BASE_URL}/products?${query}`);
+  const res = await axiosInstance.get(`/products?${query}`);
 
-  if (!response.ok) {
-    throw new Error("상품을 불러오는 데 실패했습니다.");
-  }
-  const body = await response.json();
-  return body;
+  return res.data;
 }
 
 // 디테일 상품 정보
@@ -21,20 +17,14 @@ export async function getProductId(
   >,
   setLoading: React.Dispatch<React.SetStateAction<boolean>>
 ) {
-  const response = await fetch(`${BASE_URL}/products/${productId}`);
-
   try {
-    const body = await response.json();
-    setProductData(body);
-  } catch (error) {
-    console.log(error);
+    const res = await axiosInstance.get(`/products/${productId}`);
+    setProductData(res.data);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    setLoading(false);
   }
-
-  if (!response.ok) {
-    throw new Error("정보를 불러오는 데 실패했습니다.");
-  }
-
-  setLoading(false);
 }
 
 // 디테일 댓글
@@ -44,17 +34,12 @@ export async function getComments(
     React.SetStateAction<CommentDataProps | undefined>
   >
 ) {
-  const response = await fetch(
-    `${BASE_URL}/products/${productId}/comments?limit=10`
-  );
   try {
-    const body = await response.json();
-    setCommentsData(body);
-  } catch (error) {
-    console.log(error);
-  }
-
-  if (!response.ok) {
-    throw new Error("정보를 불러오는 데 실패했습니다.");
+    const res = await axiosInstance.get(
+      `/products/${productId}/comments?limit=10`
+    );
+    setCommentsData(res.data);
+  } catch (err) {
+    console.error(err);
   }
 }
--- src/api/types.ts
+++ src/api/types.ts
@@ -1,4 +1,4 @@
-import { StaticImport, StaticRequire } from "next/dist/shared/lib/get-img-props";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
 export interface ProductDataProps {
   createdAt: string;
