497ee0ad809787629a81ea107eaf2251cbda1c52
Nerim <71241364+y5037@users.noreply.github.com>
2025-04-20 20:43:36 +0900

    fix: 로그인 세션 만료 안내 모달이 두번씩 호출되어 수정 (#20)

--- src/utils/useRefreshToken.ts
+++ src/utils/useRefreshToken.ts
@@ -1,16 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useAuth } from "./useAuth";
 import { setupInterceptors } from "../api/axiosInstance";
 
 export function useRefreshToken() {
   const { showModal, setShowModal, isModalMessage, setIsModalMessage } =
     useAuth();
+  const hasSetMessage = useRef(false);
 
   useEffect(() => {
     if (!showModal) return;
 
     if (showModal) {
       setIsModalMessage("인증이 만료되어 로그인 화면으로 이동합니다.");
+      hasSetMessage.current = true;
     }
   }, [showModal, setIsModalMessage]);
 
