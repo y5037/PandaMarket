48db8bbf2f42000ea0f3696147516fa3063cd73e
Nerim <71241364+y5037@users.noreply.github.com>
2025-04-20 20:35:59 +0900

    feat: 상품 좋아요 기능 구현 및 상품 디테일 API 기존 코드 react query로 변경 (#19)

--- src/api/productApi.ts
+++ src/api/productApi.ts
@@ -1,5 +1,4 @@
 import axiosInstance from "./axiosInstance";
-import { CommentDataProps, ProductDataProps } from "./types";
 
 // 베스트/전체 상품 리스트
 export async function getProductData(params = {}) {
@@ -7,39 +6,4 @@ export async function getProductData(params = {}) {
   const res = await axiosInstance.get(`/products?${query}`);
 
   return res.data;
-}
-
-// 디테일 상품 정보
-export async function getProductId(
-  productId: string | string[],
-  setProductData: React.Dispatch<
-    React.SetStateAction<ProductDataProps | undefined>
-  >,
-  setLoading: React.Dispatch<React.SetStateAction<boolean>>
-) {
-  try {
-    const res = await axiosInstance.get(`/products/${productId}`);
-    setProductData(res.data);
-  } catch (err) {
-    console.error(err);
-  } finally {
-    setLoading(false);
-  }
-}
-
-// 디테일 댓글
-export async function getComments(
-  productId: string | string[],
-  setCommentsData: React.Dispatch<
-    React.SetStateAction<CommentDataProps | undefined>
-  >
-) {
-  try {
-    const res = await axiosInstance.get(
-      `/products/${productId}/comments?limit=10`
-    );
-    setCommentsData(res.data);
-  } catch (err) {
-    console.error(err);
-  }
 }
---
+++ src/hooks/useGetProductDetail.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "../api/axiosInstance";
+
+const fetchProduct = async (productId: string) => {
+  try {
+    const response = await axiosInstance.get(`/products/${productId}`);
+
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export const useGetProductDetail = (productId: string) => {
+  return useQuery({
+    queryKey: ["product", productId],
+    queryFn: () => fetchProduct(productId),
+  });
+};
